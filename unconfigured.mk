b36fbe0d422685b4dae2f36bce8a5e06de37ff88
Webb Roberts <webb.roberts@gtri.gatech.edu>
2015-10-19 17:55:11 -0400

    rebooted build system

--- unconfigured.mk
+++ unconfigured.mk
@@ -1,12 +1,18 @@
 
-default:
-	@ printf "No default target. Targets:\n"
-	@ printf "    zero: remove everything that can be generated\n"
-	@ printf "    init: create everything that can be generated pre-configure\n"
+.PHONY: all # create everything that can be generated pre-configure
+all: install-sh
+	autoreconf --install --verbose
 
-zero:
-	$(RM) -r .gradle autom4te.cache out
-	$(RM) Makefile autm4te.cache autoscan.log config.log configure 
+.PHONY: help # print this help
+help:
+	@ echo Not doing anything. Maybe you wanted one of these targets:
+	@ sed -e 's/^.PHONY: *\([^ #]*\) *\# *\(.*\)$$/  \1: \2/p;d' unconfigured.mk
+
+.PHONY: clean # remove everything that can be generated
+clean: 
+	$(RM) -r .gradle autom4te.cache out tmp
+	$(RM) Makefile stow.mk configure
+	$(RM) autm4te.cache autoscan.log config.log configure 
 	$(RM) config.guess config.sub config.status install-sh ltmain.sh
 	$(RM) depcomp missing aclocal.m4 
 	find . -type f -name '*~' -delete
@@ -15,5 +21,3 @@ install-sh:
 	glibtoolize -icf
 	$(RM) ltmain.sh config.guess config.sub
 
-init: install-sh
-	autoreconf --install --verbose
